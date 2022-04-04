410b2f52cd15a5477dc4b506b7e30fd36cc9e0a4
Akshay Neje <akshayneje2823@gmail.com>
2022-04-05 03:08:48 +0530

    Just Stated

---
+++ main.js
@@ -0,0 +1,23 @@
+let addToDobutton = document.getElementById("addToDo");
+
+let toDoContainer = document.getElementById("toDoContainer");
+
+let inputField = document.getElementById("inputField");
+
+addToDobutton.addEventListener("click", function(){
+
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+
+    paragraph.innerHTML = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+    paragraph.addEventListener("click",function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener("dblclick",function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
